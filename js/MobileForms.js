a97e46a8057245044831f0ec2d0328a8d139b23b
Dipali <dipssonawane123@gmail.com>
2022-08-06 01:09:35 +0530

    Modified:javaScript for password validation is added

--- js/MobileForms.js
+++ js/MobileForms.js
@@ -9,7 +9,7 @@ const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input',function(){
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if(nameRegex.test(text.value)){ 
+    if(nameRegex.test(text.value)){
         textError.textContent = "";
     }else{
         textError.textContent = "Name is incorrect";
@@ -29,11 +29,22 @@ email.addEventListener('input',function(){
 
 const phone = document.querySelector('#tel');
 const phoneError = document.querySelector('.tel-error');
-phone.addEventListener('input',function(){
+phone.addEventListener('input',function(){ 
     let phoneRegex = RegExp('^([0-9]{2}[ ][0-9]{10})$');
     if(phoneRegex.test(phone.value)){
         phoneError.textContent = "";
     }else{
         phoneError.textContent = "Phone number is incorrect";
     }
-})
+});
+
+const pass = document.querySelector('#pwd');
+const passError = document.querySelector('.pass-error');
+pass.addEventListener('input',function(){
+    let passRegex = RegExp('[A-z0-9@#$%*]{8,}$');
+    if(passRegex.test(pass.value)){
+        passError.textContent = "";
+    }else{
+        passError.textContent = "Password is incorrect";
+    }
+}); 
