d43e8825710191683d3463dbfaf042fd9cbb8f66
Dipali <dipssonawane123@gmail.com>
2022-08-06 00:51:15 +0530

    Modified:REgex pattern

--- js/MobileForms.js
+++ js/MobileForms.js
@@ -4,7 +4,7 @@ output.textContent = salary.value;
 salary.addEventListener('input', function(){
     output.textContent = salary.value;
 });
-
+ 
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input',function(){
@@ -15,3 +15,14 @@ text.addEventListener('input',function(){
         textError.textContent = "Name is incorrect";
     }
 });
+
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',function(){
+    let emailRegex = RegExp('^[a-zA-Z0-9_]+[-+.]?[A-Za-z0-9_]+@[A-Za-z0-9]+[.][a-z]{2,}[.]?([a-z]{2,})?$');
+    if(emailRegex.test(email.value)){
+        emailError.textContent = "";
+    }else{
+        emailError.textContent = "Email is incorrect";
+    }
+})
